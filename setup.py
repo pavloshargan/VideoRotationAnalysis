188e40978875babc90c0bbd307c7943a3288af64
Pavlo Shargan <pavel.shargan@gmail.com>
2023-08-26 17:59:11 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="videorotation",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "opencv-python",
+        "numpy",
+        "tensorflow"
+    ],
+    package_data={
+        'videorotation': ['rotnet_street_view_resnet50_keras2_pb/*'],
+    },
+    author="Pavlo Sharahn",
+    author_email="activetechinnovators@example.com",
+    description="A module to analyze video rotation.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/videorotation",
+)
